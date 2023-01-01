88d66da2f965a00e671f3995486712e2c1456695
RockyBaura <251drougnov@gmail.com>
2023-01-01 10:59:09 +0600

    Darkmode feature added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import React from "react";
+import Header from './components/Header';
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(true);
+  
+  const toggleDarkMode =()=>{
+    setDarkMode(prevMode => !prevMode);
+  }
+
   return (
     <div>
-      
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
     </div>
   );
 }
