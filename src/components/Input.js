2acc7e3c66a9b461b5c9aa04fefe68057f17fc16
RockyBaura <251drougnov@gmail.com>
2023-01-01 20:45:38 +0600

    Input initial update

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-const Input = () => {
+const Input = (props) => {
+
+  const [input, setInput] = React.useState('');
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    setInput('');
+
+    props.onSubmit(
+      {
+        text: input
+      }
+    )
+  }
+  const handleChange =(e)=>{
+    setInput(e.target.value);
+  }
+
   return (
-    <div>Input</div>
+    <form onSubmit={handleSubmit}>
+      <button type='submit'></button>
+      <input type="text" placeholder="Create a new todo..." name='text' value={input} onChange={handleChange} autoFocus/>
+    </form>
   )
 }
 
