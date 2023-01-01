f0e8ea77f075b4eb79bae0338c96d675fb388271
RockyBaura <251drougnov@gmail.com>
2023-01-01 10:59:54 +0600

    Header jsx complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import darkLogo from '../images/icon-moon.svg';
+import lightLogo from '../images/icon-sun.svg';
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>Header</div>
+    <header className={props.darkMode ? 'dark' : ''}>
+      <h1 className="logo">TODO</h1>
+      <button onClick={props.toggleDarkMode}>{props.darkMode ? <img src={darkLogo} alt=""/>:<img src={lightLogo} alt=""/>}</button>
+    </header>
   )
 }
 
