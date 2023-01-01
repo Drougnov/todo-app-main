d1fcaf7a49c269ac1a62c1915ec45d1bae0396b2
RockyBaura <251drougnov@gmail.com>
2023-01-01 10:05:11 +0600

    Components added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Drag and drop to reorder List</p>
+            <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/Drougnov" target="_blank" rel="noreferrer">Rocky Barua</a>.</p>
+        </footer>
+    )
+}
+
+export default Footer;
