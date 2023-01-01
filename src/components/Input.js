272fdaeee473e968635ccc4f499b1dbc78ace7c8
RockyBaura <251drougnov@gmail.com>
2023-01-01 21:22:13 +0600

    Nanoid added

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 
 const Input = (props) => {
 
@@ -10,6 +11,7 @@ const Input = (props) => {
 
     props.onSubmit(
       {
+        id: nanoid(),
         text: input
       }
     )
