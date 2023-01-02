ad3dd5d4eaad9f2e818362214d0530d40cf1de28
RockyBaura <251drougnov@gmail.com>
2023-01-02 11:12:35 +0600

    Dark mode added to input component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from './components/Header';
+import Input from './components/Input';
 
 function App() {
 
@@ -12,6 +13,9 @@ function App() {
   return (
     <div>
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <main>
+        <Input darkMode={darkMode} />
+      </main>
     </div>
   );
 }
