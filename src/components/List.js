7064e9205e860395be54c119f428bfba3af811da
RockyBaura <251drougnov@gmail.com>
2023-01-03 22:00:21 +0600

    List style complete [except filter]

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import { useMedia } from 'react-use';
+import crossIcon from '../images/icon-cross.svg';
 
-const List = () => {
+const List = (props) => {
+
+  const isLarge = useMedia('(min-width: 750px)');
+  console.log(props.todoList)
+
+  const todoList = props.todoList.map(item =>(
+    <li key={item.id}>
+      <button type='submit' aria-label='add todo' className='add-btn'></button>
+      <span>{item}</span>
+      <button className='delete-btn'><img src={crossIcon} alt=""/></button>
+    </li>
+  ))
+
   return (
-    <div>List</div>
+    <div className={props.darkMode ? 'list-container dark':'list-container'}>
+      <ul>
+        {todoList}
+      </ul>
+      <div className='status'>
+          <span>{props.todoList.length} items left</span>
+          {isLarge && (
+            <div>
+              <button>All</button>
+              <button>Active</button>
+              <button>Completed</button>
+            </div>
+          )}
+          <button className='clear-btn'>Clear completed</button>
+        </div>
+      {!isLarge && (
+          <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+        )}
+    </div>
   )
 }
 
