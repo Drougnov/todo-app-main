92111e3d55464fc98569d320883d054d53ca47d1
RockyBaura <251drougnov@gmail.com>
2023-01-04 11:36:12 +0600

    delete-todo, no-todo feature added

--- src/components/List.js
+++ src/components/List.js
@@ -5,19 +5,19 @@ import crossIcon from '../images/icon-cross.svg';
 const List = (props) => {
 
   const isLarge = useMedia('(min-width: 750px)');
-  console.log(props.todoList)
 
   const todoList = props.todoList.map(item =>(
     <li key={item.id}>
       <button type='submit' aria-label='add todo' className='add-btn'></button>
-      <span>{item}</span>
-      <button className='delete-btn'><img src={crossIcon} alt=""/></button>
+      <span>{item.text}</span>
+      <button className='delete-btn' onClick={(e)=>{props.deleteTodo(e, item.id)}}><img src={crossIcon} alt=""/></button>
     </li>
   ))
 
   return (
     <div className={props.darkMode ? 'list-container dark':'list-container'}>
       <ul>
+        {props.todoList.length === 0 && <li className='no-todo'>All caught up!</li>}
         {todoList}
       </ul>
       <div className='status'>
