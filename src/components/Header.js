2b8569da32f6903a616d1bb3ee17a41b20d9ea7c
RockyBaura <251drougnov@gmail.com>
2023-01-07 22:50:08 +0600

    'header-nav' added for centering

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ import lightLogo from '../images/icon-sun.svg';
 const Header = (props) => {
   return (
     <header className={props.darkMode ? 'dark' : ''}>
-      <h1 className="logo">TODO</h1>
-      <button className='toggle-btn' onClick={props.toggleDarkMode}>{props.darkMode ? <img src={lightLogo} alt=""/>:<img src={darkLogo} alt=""/>}</button>
+      <div className="header-nav">
+        <h1 className="logo">TODO</h1>
+        <button className='toggle-btn' onClick={props.toggleDarkMode}>{props.darkMode ? <img src={lightLogo} alt=""/>:<img src={darkLogo} alt=""/>}</button>
+      </div>
     </header>
   )
 }
