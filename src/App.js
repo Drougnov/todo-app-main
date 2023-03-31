99ffec604e4a42e37f218bfd58d94ecf1146a198
RockyBaura <251drougnov@gmail.com>
2023-03-31 12:47:49 +0600

    darkMode on user preference|pass setTodoList props

--- src/App.js
+++ src/App.js
@@ -1,63 +1,73 @@
-import React from "react";
-import Header from './components/Header';
-import Input from './components/Input';
-import List from './components/List';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Input from "./components/Input";
+import List from "./components/List";
+import Footer from "./components/Footer";
 
 function App() {
+    const [darkMode, setDarkMode] = useState(
+        window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+    );
+    const [todoList, setTodoList] = useState(
+        () => JSON.parse(localStorage.getItem("todoList")) || []
+    );
 
-  const [darkMode, setDarkMode] = React.useState(true);
-  const [todoList, setTodoList] = React.useState(()=> JSON.parse(localStorage.getItem("todoList")) || []);
+    useEffect(() => {
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    }, [todoList]);
 
-  React.useEffect(()=>{
-    localStorage.setItem("todoList", JSON.stringify(todoList))
-  },[todoList])
+    const toggleDarkMode = () => {
+        setDarkMode((prevMode) => !prevMode);
+    };
 
-  const toggleDarkMode =()=>{
-    setDarkMode(prevMode => !prevMode);
-  }
+    const addTodo = (todo) => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            //check if the current todo is empty or only white space
+            return;
+        }
+        todo["text"] = todo["text"].replace(/\s+/g, " "); //trim extra spaces
+        setTodoList([...todoList, todo]);
+    };
 
-  const addTodo = (todo) =>{
-    if(!todo.text || /^\s*$/.test(todo.text)){ //check if the current todo is empty or only white space
-      return;
-    }
-    todo["text"] = todo["text"].replace(/\s+/g, ' ');  //trim extra spaces
-    setTodoList([...todoList, todo]);
-  }
+    const deleteTodo = (event, todoId) => {
+        event.stopPropagation();
+        setTodoList((oldTodos) =>
+            oldTodos.filter((todo) => todo.id !== todoId)
+        );
+    };
 
-  const deleteTodo=(event, todoId)=>{
-    event.stopPropagation();
-    setTodoList(oldTodos => oldTodos.filter(todo => todo.id !== todoId))
-  }
+    const toggleCompleted = (todoId) => {
+        const updatedTodoList = todoList.map((todo) => {
+            if (todo.id === todoId) {
+                return { ...todo, completed: !todo.completed };
+            }
+            return todo;
+        });
+        setTodoList(updatedTodoList);
+    };
 
-  const toggleCompleted=(todoId)=>{
-    const updatedTodoList = todoList.map(todo => {
-      if (todo.id === todoId) {
-        return {...todo, completed: !todo.completed};
-      }
-      return todo;
-    });
-    setTodoList(updatedTodoList);
-  }
+    const clearCompleted = () => {
+        setTodoList((oldTodos) => oldTodos.filter((todo) => !todo.completed));
+    };
 
-  const clearCompleted=()=>{
-    setTodoList(oldTodos => oldTodos.filter(todo => !todo.completed))
-  }
-
-  return (
-    <div className={darkMode ? 'container dark' : 'container'}>
-      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-      <main>
-        <Input darkMode={darkMode} onSubmit={addTodo} />
-        <List darkMode={darkMode}
-              todoList={todoList}
-              deleteTodo={deleteTodo}
-              toggleCompleted={toggleCompleted}
-              clearCompleted={clearCompleted}/>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className={darkMode ? "container dark" : "container"}>
+            <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <main>
+                <Input darkMode={darkMode} onSubmit={addTodo} />
+                <List
+                    darkMode={darkMode}
+                    todoList={todoList}
+                    setTodoList={setTodoList}
+                    deleteTodo={deleteTodo}
+                    toggleCompleted={toggleCompleted}
+                    clearCompleted={clearCompleted}
+                />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
